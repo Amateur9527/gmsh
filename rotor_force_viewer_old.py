45330fd178806f911b3b4963ea0641175c23351c
xiao <prandtlair@gmail.com>
2023-04-26 16:12:58 +0800

    new

--- rotor_force_viewer_old.py
+++ rotor_force_viewer_old.py
@@ -4,13 +4,9 @@ import sys
 import numpy as np
 from matplotlib import pyplot as plt
 import warnings
-import math
 ###define calculate parameters
-a = 340
 time = 0.05
 Frames = 10
-omega = 35
-u = 10
 
 if __name__ == '__main__':
     if len(sys.argv) < 5:
@@ -22,9 +18,7 @@ if __name__ == '__main__':
     n_part = int(sys.argv[4])
     Forces = np.array(range(33)).reshape(11,3)
     time_step = time / Frames
-    k = omega * 2 * math.pi
-    mach = format(k / a, '.2f')
-    μ = format(u / k, '.2f')
+    #sig = len(time_step) - 2 
     horizon = []
     i = 0
     for frame in range(Frames+1):
@@ -50,25 +44,25 @@ if __name__ == '__main__':
                 weights[i_row] = row[6]
                 i_row += 1
         Forces[i_frame] = np.matmul(forces.T,weights)
-        assert i_row == n_row
-        force_norm_max = 0.0
-        for force in forces:
-            force_norm_max = max(force_norm_max, np.linalg.norm(force))
-        forces /= force_norm_max
-        forces *= 0.5
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
-        ax.set_aspect('equal')
-        ax.quiver(points[:,0], points[:,1], points[:,2],
-            forces[:,0], forces[:,1], forces[:,2])
-            # forces[:,0], forces[:,1], forces[:,2], length=0.2, normalize=True)
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-        ax.plot([1], [0], [0])
-        plt.title(f'Frame[{i_frame}]')
-        plt.show()
-    
+
+    # fig, ax = plt.subplot()
+    # ax.plot(Forces)
+    # plt.show()
+    #     i += 1
+    #     print(forces.shape)
+    #     print(weights.shape)
+    #print(Forces)
+        # assert i_row == n_row
+        # fig = plt.figure()
+        # ax = fig.add_subplot(projection='3d')
+        # ax.set_aspect('equal')
+        # ax.quiver(points[:,0], points[:,1], points[:,2],
+        #     forces[:,0], forces[:,1], forces[:,2], length=0.2, normalize=True)
+        # ax.set_xlabel('X')
+        # ax.set_ylabel('Y')
+        # ax.set_zlabel('Z')
+        # plt.title(f'Frame[{i_frame}]')
+        # plt.show()
     plt.plot(horizon, Forces[:, 2], linewidth=1)
     plt.scatter(horizon,Forces[:, 2], c='red', s=20)
     plt.title('Lift-Time', fontsize=24)
@@ -77,4 +71,3 @@ if __name__ == '__main__':
     for frame in range(Frames+1):
         plt.text(horizon[frame], Forces[frame, 2], (horizon[frame], Forces[frame, 2]), fontsize=8)
     plt.show()
-    print(f'time={time}, Frames={Frames}, μ={μ}, mach={mach}')
